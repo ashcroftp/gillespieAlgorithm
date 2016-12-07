86f082293b4f6f411f2ab6b7d6e95bb3009fea7c
Peter Ashcroft <peter.ashcroft@env.ethz.ch>
2016-12-07 15:23:24 +0100

    Define header file to contain propensity functions and stoichiometry

---
+++ myProcess.h
@@ -0,0 +1,10 @@
+//myProcess.h
+//=========================================
+// Guard
+#ifndef __MYPROCESS_H_INCLUDED__
+#define __MYPROCESS_H_INCLUDED__ 
+//=========================================
+
+class X { };
+
+#endif 
