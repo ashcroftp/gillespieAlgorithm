4c96108ea819c15a4ecdaa416d258e908bc83e19
Peter Ashcroft <peter.ashcroft@env.ethz.ch>
2016-12-07 23:40:10 +0100

    New header file structure

---
+++ outputData.h
@@ -0,0 +1,10 @@
+//outputData.h
+//=========================================
+// Guard
+#ifndef __OUTPUTDATA_H_INCLUDED__
+#define __OUTPUTDATA_H_INCLUDED__ 
+//=========================================
+
+class X { };
+
+#endif 
